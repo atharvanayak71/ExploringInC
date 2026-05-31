ce65a80c4cafb9e827b807baaee5ede94248ca2b
Atharva Nayak <atharvanayak2006@gmail.com>
2026-06-01 02:30:33 +0530

    Solved Exercise 5

---
+++ THE ABC OF C/Exercise5.c
@@ -0,0 +1,12 @@
+#include<stdio.h>
+int main(){
+    int a = 10, b = 20;
+    printf("Before interchanging\n");
+    printf("a= %d b = %d\n",a,b);
+    a = a+b;
+    b = a-b;
+    a = a-b;
+    printf("After interchanging\n");
+    printf("a= %d b = %d\n", a,b);
+    return 0;
+}
