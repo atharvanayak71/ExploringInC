8988617fd77f18f1ca7cce28a0cda59f58c79acc
Atharva Nayak <atharvanayak2006@gmail.com>
2026-06-01 15:09:37 +0530

    Exercise 17 solved

---
+++ STEERING THE CONTROL/Exercise17.c
@@ -0,0 +1,11 @@
+//Atharva Nayak
+//0905CS241063
+
+#include<stdio.h>
+int main(){
+    int x = 10, y = 5, p, q;
+    p = x>9;
+    q = x>3 && y!=3;
+    printf("p = %d q = %d\n", p, q);
+    return 0;
+}
