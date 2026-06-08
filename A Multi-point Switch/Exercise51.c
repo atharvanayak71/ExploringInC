6a2dd8ac58833ea12678bc6fc0f025b030b54e71
Atharva Nayak <atharvanayak2006@gmail.com>
2026-06-08 19:44:38 +0530

    Ignore exe files

---
+++ A Multi-point Switch/Exercise51.c
@@ -0,0 +1,13 @@
+// Atharva Nayak
+// 0905CS241063
+
+#include<stdio.h>
+#include<stdlib.h>
+int main(){
+    printf("Hope is Hoping against the hope...\n");
+    goto hope;
+    printf("Even if it seems hopeless.\n");
+    hope:
+    exit(0);
+    return 0;
+}
